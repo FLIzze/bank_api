a8aacec60ffcae0e95d1217cc8eb0f7edb6cac6f
fmael <mael.fath@ynov.com>
2024-12-05 14:14:21 +0100

    Fix controller function names

--- src/advisors/advisors.controller.ts
+++ src/advisors/advisors.controller.ts
@@ -9,27 +9,27 @@ export class AdvisorsController {
     constructor(private readonly AdvisorsService: AdvisorsService) {}
 
     @Post()
-    create(@Body() CreateAdvisorsDto: CreateAdvisorsDto) {
+    async create(@Body() CreateAdvisorsDto: CreateAdvisorsDto) {
         return this.AdvisorsService.create(CreateAdvisorsDto);
     }
 
     @Get(':advisorId')
-    getAdvisor(@Param('advisorId') advisorId: string) {
+    async findOne(@Param('advisorId') advisorId: string) {
         return this.AdvisorsService.findOne(advisorId);
     }
 
     @Get(':advisorId/clients')
-    getAdvisorClients(@Param('advisorId') advisorId: string) {
+    async findClients(@Param('advisorId') advisorId: string) {
         return this.AdvisorsService.findClients(advisorId);
     }
 
     @Put(':advisorId')
-    updateAdvisor(@Param('advisorId') advisorId: string, @Body() updateAdvisorDto: any) {
+    async update(@Param('advisorId') advisorId: string, @Body() updateAdvisorDto: any) {
         return this.AdvisorsService.update(advisorId, updateAdvisorDto);
     }
 
     @Delete(':advisorId')
-    deleteAdvisor(@Param('advisorId') advisorId: string) {
+    async delete(@Param('advisorId') advisorId: string) {
         return this.AdvisorsService.delete(advisorId);
     }
 }

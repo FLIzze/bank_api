783b40edee2ee4f82aa228ca964a3188cbb761ff
fmael <mael.fath@ynov.com>
2024-12-05 14:08:24 +0100

    Advisors controller create, findOne, update, delete, findClients

--- src/advisors/advisors.controller.ts
+++ src/advisors/advisors.controller.ts
@@ -1,4 +1,35 @@
 import { Controller } from '@nestjs/common';
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AdvisorsService } from './advisors.service';
+import { CreateAdvisorsDto } from './dto/create-advisors.dto';
 
 @Controller('advisors')
-export class AdvisorsController {}
+export class AdvisorsController {
+
+    constructor(private readonly AdvisorsService: AdvisorsService) {}
+
+    @Post()
+    create(@Body() CreateAdvisorsDto: CreateAdvisorsDto) {
+        return this.AdvisorsService.create(CreateAdvisorsDto);
+    }
+
+    @Get(':advisorId')
+    getAdvisor(@Param('advisorId') advisorId: string) {
+        return this.AdvisorsService.findOne(advisorId);
+    }
+
+    @Get(':advisorId/clients')
+    getAdvisorClients(@Param('advisorId') advisorId: string) {
+        return this.AdvisorsService.findClients(advisorId);
+    }
+
+    @Put(':advisorId')
+    updateAdvisor(@Param('advisorId') advisorId: string, @Body() updateAdvisorDto: any) {
+        return this.AdvisorsService.update(advisorId, updateAdvisorDto);
+    }
+
+    @Delete(':advisorId')
+    deleteAdvisor(@Param('advisorId') advisorId: string) {
+        return this.AdvisorsService.delete(advisorId);
+    }
+}
--- src/advisors/advisors.service.ts
+++ src/advisors/advisors.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Advisors } from './interfaces/advisors.interface';
 
 @Injectable()
-export class AdvisorsService {}
+export class AdvisorsService {
+    constructor( 
+        @Inject('ADVISORS_MODEL') private advisorModel: Model<Advisors>,
+    ) {}
+
+    async create(createAdvisorDto: Advisors): Promise<Advisors> {
+        const createdAdvisor = new this.advisorModel(createAdvisorDto);
+        return createdAdvisor.save();
+    }
+
+    async findAll(): Promise<Advisors[]> {
+        return this.advisorModel.find().exec();
+    }
+
+    async findOne(advisorId: string): Promise<Advisors> {
+        return this.advisorModel.findById(advisorId).exec();
+    }
+
+    async update(advisorId: string, createAdvisorDto: Advisors): Promise<Advisors> {
+        return this.advisorModel.findOneAndUpdate({advisorId}, createAdvisorDto, {new:true}).exec();
+    }
+
+    async delete(advisorId: string): Promise<Advisors> {
+        return this.advisorModel.findOneAndDelete({advisorId}).exec();
+    }
+
+    async findClients(advisorId: string): Promise<Advisors> {
+        return this.advisorModel.findOne({advisorId}).populate('clients').exec();
+    }
+}

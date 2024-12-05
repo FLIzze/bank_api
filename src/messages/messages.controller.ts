42a5d66db75b0749be2e48d80cc031b51cc537b9
fmael <mael.fath@ynov.com>
2024-12-05 14:21:47 +0100

    Messages controller create, findByClient, findByAdvisor, update

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+import { CreateMessagesDto } from './dto/create-messages.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    constructor(private readonly MessagesService: MessagesService) {}
+
+    @Post()
+    async create(@Body() CreateMessagesDto:CreateMessagesDto) {
+        return this.MessagesService.create(CreateMessagesDto);
+    }
+
+    @Get('client/:clientId')
+    async findByClient(@Param('clientId') clientId: string) {
+        return this.MessagesService.findByClient(clientId);
+    }
+
+    @Get('advisor/:advisorId')
+    async findByAdvisor(@Param('advisorId') advisorId: string) {
+        return this.MessagesService.findByAdvisor(advisorId);
+    }
+
+    @Put(':messageId')
+    async update(@Param('messageId') messageId: string, @Body() CreateMessagesDto: CreateMessagesDto) {
+        return this.MessagesService.update(messageId, CreateMessagesDto);
+    }
+}
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Messages } from './interfaces/messages.interface';
+import { CreateMessagesDto } from './dto/create-messages.dto';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+    constructor(
+        @Inject('MESSAGES_MODEL') 
+        private readonly messagesModel: Model<Messages>
+    ) {}
+
+    async create(CreateMessagesDto:CreateMessagesDto):Promise<Messages> {
+        const newMessage = new this.messagesModel(CreateMessagesDto);
+        return newMessage.save();
+    }
+
+    async findByClient(clientId: string): Promise<Messages[]> {
+        return this.messagesModel.find({ clientId }).exec();
+    }
+
+    async findByAdvisor(advisorId: string): Promise<Messages[]> {
+        return this.messagesModel.find({ advisorId }).exec();
+    }
+
+    async update(messageId: string, CreateMessagesDto: CreateMessagesDto): Promise<Messages> {
+        return this.messagesModel.findByIdAndUpdate(messageId, CreateMessagesDto, { new: true }).exec();
+    }
+}

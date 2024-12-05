05e4b34dcde0e57cc74d8e603af6b8997bfe1319
fmael <mael.fath@ynov.com>
2024-12-05 14:24:40 +0100

    Fix module providers: ...MessagesProviders

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { MessagesProviders } from './messages.providers';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [MessagesController],
-    providers: [MessagesService],
+    providers: [MessagesService, ...MessagesProviders],
     exports: [MessagesService],
 })
 export class MessagesModule {}

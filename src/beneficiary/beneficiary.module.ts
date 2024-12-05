284b2c4e10b7c1e4fbeda51ac486db40da9aa481
rthimoth <thimothee.ranvin@hotmail.com>
2024-12-05 11:54:35 +0100

    add imports in beneficiary module

--- src/beneficiary/beneficiary.module.ts
+++ src/beneficiary/beneficiary.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BeneficiaryService } from './beneficiary.service';
 import { BeneficiaryController } from './beneficiary.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [BeneficiaryService],
-  controllers: [BeneficiaryController]
+  controllers: [BeneficiaryController],
+  exports: [BeneficiaryService],
 })
 export class BeneficiaryModule {}

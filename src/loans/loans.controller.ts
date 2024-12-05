67e3268da7697adbf401afde92651aea6f6338c3
fmael <mael.fath@ynov.com>
2024-12-05 15:27:22 +0100

    Loans controller create, findOne, findAllByClient, update, delete

--- src/loans/loans.controller.ts
+++ src/loans/loans.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { LoansService } from './loans.service';
+import { CreateLoansDto } from './dto/create-loans.dto';
 
 @Controller('loans')
-export class LoansController {}
+export class LoansController {
+    constructor(private readonly LoansService: LoansService) {}
+
+    @Post()
+    async create(@Body() CreateLoansDto:CreateLoansDto) {
+        return this.LoansService.create(CreateLoansDto);
+    }
+
+    @Get()
+    async findOne(loanId: string) {
+        return this.LoansService.findOne(loanId);
+    }
+
+    @Get('client/:clientId')
+    async findAllByClient(clientId: string) {
+        return this.LoansService.findAllByClient(clientId);
+    }
+
+    @Put(':loanId')
+    async update(loanId: string, @Body() CreateLoansDto: CreateLoansDto) {
+        return this.LoansService.update(loanId, CreateLoansDto);
+    }
+
+    @Delete(':loanId')
+    async delete(loanId: string) {
+        return this.LoansService.delete(loanId);
+    }
+}
--- src/loans/loans.service.ts
+++ src/loans/loans.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Loans } from './interfaces/loans.interface';
 
 @Injectable()
-export class LoansService {}
+export class LoansService {
+    constructor(
+        @Inject('LOANS_MODEL') 
+        private readonly loanModel: Model<Loans>,
+    ) {}
+
+    async create(loanData: Loans): Promise<Loans> {
+        const newLoan = new this.loanModel(loanData);
+        return newLoan.save();
+    }
+
+    async findOne(loanId: string): Promise<Loans> {
+        return this.loanModel.findById(loanId).exec();
+    }
+
+    async findAllByClient(clientId: string): Promise<Loans[]> {
+        return this.loanModel.find({ clientId }).exec();
+    }
+
+    async update(loanId: string, loanData: Loans): Promise<Loans> {
+        return this.loanModel.findByIdAndUpdate(loanId, loanData, { new: true }).exec();
+    }
+
+    async delete(loanId: string): Promise<Loans> {
+        return this.loanModel.findByIdAndDelete(loanId).exec();
+    }
+
+}

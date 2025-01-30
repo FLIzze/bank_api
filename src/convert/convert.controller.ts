647387fe6e7751c0296a5ae82e09f0abdd47cfc9
fmael <mael.fath@ynov.com>
2025-01-30 14:22:52 +0100

    Add Public to convert controller

--- src/convert/convert.controller.ts
+++ src/convert/convert.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ConvertService } from './convert.service';
 import { ConvertDto } from './dto/convert.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Public } from 'src/auth/public-strategy';
 
 @ApiTags('Convert')
 @Controller('api/convert')
 export class ConvertController {
   constructor(private readonly convertService: ConvertService) {}
 
+  @Public()
   @Post()
   @ApiOperation({ summary: 'Convert one currency to another' })
   @ApiResponse({ status: 200, description: 'Conversion réussie.' })
